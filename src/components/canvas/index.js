3369c7246d6a34c0ec040e8e46120af49f0e035c
Philemon da Silva Souza <Philemon.souza@supermix.com.br>
2022-02-07 13:32:07 -0300

    PARTIAL: Adding array implementation. Prototype fail

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,12 +1,51 @@
 
 //#region imports
-import React from 'react';
+import React, { useState, useEffect, useRef, useCallback }   from 'react';
 import PropTypes from 'prop-types';
-import {useCanvas} from "./canvasManager";
+import {draw, useCanvas} from "./canvasManager";
+import useWindowDimensions from "../../general/windowDimension";
 //#endregion
 
 const Canvas = () => {
-    const [ coordinate, setCoordinate, canvasRef, canvasWidth, canvasHeight ] = useCanvas();
+
+    const canvasRef = useRef(null);
+    const [canvas, setCanvas] =  useState(null);
+
+    const cellSide = 200;
+
+    const [map, setMap] = useState([
+        ['R', 'Y', 'R'],
+        ['Y', 'R', 'Y'],
+        ['R', 'Y', 'R']
+    ])
+
+    const [coordinate, setCoordinate] = useState({x:null,y:null});
+
+
+    const { height, width } = useWindowDimensions();
+    const [canvasWidth, setCanvasWidth] = useState(height);
+    const [canvasHeight, setCanvasHeight] = useState(width);
+
+    useEffect(()=>{
+        const canvasObj = canvasRef.current;
+        setCanvas(canvasObj.getContext('2d'));
+    },[])
+
+    useEffect(()=>{
+        for (let i = 0; i < map.length; i++) {
+            for (let j = 0; j < map[i].length; j++) {
+                let x = j * cellSide;
+                let y = i * cellSide;
+
+                let cellColor = '#e74c3c';
+                if (map[i][j] === 'Y') cellColor = '#f1c40f';
+
+                canvas.beginPath();
+                canvas.fillStyle = cellColor;
+                canvas.fillRect(x, y, cellSide, cellSide);
+            }
+        }
+    }, [canvas, map]);
 
     function getCursorPosition(event) {
         const rect = canvasRef.current.getBoundingClientRect();
@@ -17,11 +56,10 @@ const Canvas = () => {
 
         console.log("x: " + x + " y: " + y)
     }
-
     return (
         <>
             <canvas
-                style={{width: '60%' , height: '80%'}}
+                //style={{width: '60%' , height: '80%'}}
                 ref={canvasRef}
                 // width='60%'
                 // height={canvasHeight}
