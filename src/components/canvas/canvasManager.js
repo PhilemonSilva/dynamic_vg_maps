b29b4dd101b4b7852de59c3cfc523e4600b40562
Philemon da Silva Souza <Philemon.souza@supermix.com.br>
2022-02-07 19:17:06 -0300

    FIX: Limpeza e refatoração do protótipo.

--- src/components/canvas/canvasManager.js
+++ src/components/canvas/canvasManager.js
@@ -1,53 +1,19 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import useWindowDimensions from "../../general/windowDimension";
+import { useRef, useEffect } from "react";
 
-// Path2D for a Heart SVG
-const heartSVG = "M0 200 v-200 h200 a100,100 90 0,1 0,200 a100,100 90 0,1 -200,0 z"
-const SVG_PATH = new Path2D(heartSVG);
+const useCanvas = (draw) => {
 
-// Scaling Constants for Canvas
-const SCALE = 0.1;
-const OFFSET = 80;
-
-// export const canvasWidth = window.innerWidth * .5;
-// export const canvasHeight = window.innerHeight * .9;
-
-export function draw(ctx, location){
-    ctx.fillStyle = 'red';
-    ctx.shadowColor = 'blue';
-    ctx.shadowBlur = 15;
-    ctx.save();
-    ctx.scale(SCALE, SCALE);
-    ctx.translate(location.x / SCALE - OFFSET, location.y / SCALE - OFFSET);
-    ctx.rotate(225 * Math.PI / 180);
-    ctx.fill(SVG_PATH);
-    // .restore(): Canvas 2D API restores the most recently saved canvas state
-    ctx.restore();
-}
-
-export function useCanvas(){
     const canvasRef = useRef(null);
-    const [coordinate, setCoordinate] = useState({x:null,y:null});
-    const { height, width } = useWindowDimensions();
-    const [canvasWidth, setCanvasWidth] = useState(height);
-    const [canvasHeight, setCanvasHeight] = useState(width);
 
-    let updateCanvasSize = useCallback(()=>{
-        setCanvasWidth(height * .5);
-        setCanvasHeight(width * .9)
-    }, [height, width]);
-    useEffect(()=>{
-        updateCanvasSize()
-    }, [updateCanvasSize]);
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.width = 600;
+        canvas.height = 600;
+        const context = canvas.getContext('2d');
 
-    useEffect(()=>{
-        const canvasObj = canvasRef.current;
-        const ctx = canvasObj.getContext('2d');
-        // clear the canvas area before rendering the coordinate held in state
-        ctx.clearRect( 0,0, canvasWidth, canvasHeight );
-        draw(ctx, coordinate);
-    });
+        draw(context)
+    }, [ draw ])
 
-    return [ coordinate, setCoordinate, canvasRef, canvasWidth, canvasHeight ];
+    return canvasRef
 }
 
+export default useCanvas
--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,78 +1,33 @@
+import React, {useRef, useEffect, useState, useCallback} from 'react'
+import useCanvas from "./canvasManager";
 
-//#region imports
-import React, { useState, useEffect, useRef, useCallback }   from 'react';
-import PropTypes from 'prop-types';
-import {draw, useCanvas} from "./canvasManager";
-import useWindowDimensions from "../../general/windowDimension";
-//#endregion
+const Canvas = props => {
 
-const Canvas = () => {
-
-    const canvasRef = useRef(null);
-    const [canvas, setCanvas] =  useState(null);
-
+    const red = '#e74c3c';
+    const yellow = '#f1c40f';
     const cellSide = 200;
 
     const [map, setMap] = useState([
-        ['R', 'Y', 'R'],
-        ['Y', 'R', 'Y'],
-        ['R', 'Y', 'R']
-    ])
+        [red, yellow, red],
+        [yellow, red, yellow],
+        [red, yellow, red]
+    ]);
 
-    const [coordinate, setCoordinate] = useState({x:null,y:null});
-
-
-    const { height, width } = useWindowDimensions();
-    const [canvasWidth, setCanvasWidth] = useState(height);
-    const [canvasHeight, setCanvasHeight] = useState(width);
-
-    useEffect(()=>{
-        const canvasObj = canvasRef.current;
-        setCanvas(canvasObj.getContext('2d'));
-    },[])
-
-    useEffect(()=>{
+    const draw = useCallback((context) => {
         for (let i = 0; i < map.length; i++) {
             for (let j = 0; j < map[i].length; j++) {
                 let x = j * cellSide;
                 let y = i * cellSide;
-
-                let cellColor = '#e74c3c';
-                if (map[i][j] === 'Y') cellColor = '#f1c40f';
-
-                canvas.beginPath();
-                canvas.fillStyle = cellColor;
-                canvas.fillRect(x, y, cellSide, cellSide);
+                context.beginPath();
+                context.fillStyle = map[i][j];
+                context.fillRect(x, y, cellSide, cellSide);
             }
         }
-    }, [canvas, map]);
+    },[[JSON.stringify(map)]])
 
-    function getCursorPosition(event) {
-        const rect = canvasRef.current.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
+    const canvasRef = useCanvas(draw)
 
-        setCoordinate({ x: x +10, y:y+10});
-
-        console.log("x: " + x + " y: " + y)
-    }
-    return (
-        <>
-            <canvas
-                //style={{width: '60%' , height: '80%'}}
-                ref={canvasRef}
-                // width='60%'
-                // height={canvasHeight}
-                onClick={getCursorPosition}
-            />
-        </>
-    );
+    return <canvas ref={canvasRef} {...props}/>
 }
 
-Canvas.propTypes = {
-}
-
-Canvas.defaultProps = {
-}
-
-export default Canvas;
+export default Canvas
