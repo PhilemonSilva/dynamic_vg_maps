0a94df3bd2034267841037cd250009e7f64b5c5e
Philemon da Silva Souza <Philemon.souza@supermix.com.br>
2022-02-09 20:49:19 -0300

    PARTIAL: Organizing map generator.

---
+++ src/scripts/mapGenerator.js
@@ -0,0 +1,26 @@
+
+const generateMap = () => {
+    let xCount = 100;
+    let yCount = 100;
+
+    let solidCell = { solid: true };
+    let emptyCell = { solid: false };
+
+    var map =[];
+    for (let x = 0; x < xCount; x++){
+        map.push([]);
+        for (let y = 0; y < yCount; y++) {
+
+            map[x].push()
+
+        }
+    }
+}
+
+const iterateOn = (array, operation) => {
+
+}
+
+
+
+export default generateMap
